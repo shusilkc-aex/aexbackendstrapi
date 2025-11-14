8d16c6ba8be821da727b7aeb94698fb900bec4f9
Shusil KC <shusilkc@appleexpress.com>
2025-11-13 19:07:16 -0500

    increase the file size limit

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -5,7 +5,17 @@ export default [
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
-  'strapi::body',
+  {
+    name: 'strapi::body',
+    config: {
+      jsonLimit: '500mb',       // increase JSON limit
+      formLimit: '500mb',       // increase form data limit
+      textLimit: '500mb',       // increase text limit
+      formidable: {
+        maxFileSize: 500 * 1024 * 1024, // 500 MB
+      },
+    },
+  },
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
