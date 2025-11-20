4ff44a637a7474430f593167b6851bd805e59196
Shusil KC <shusilkc@appleexpress.com>
2025-11-20 14:59:46 -0500

    change it back to msi

--- config/plugins.ts
+++ config/plugins.ts
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
         account: env('STORAGE_ACCOUNT_NAME'),              
         accountKey: env('STORAGE_ACCOUNT_KEY'), 
         containerName: env('BLOB_CONTAINER_NAME'),
-        authType: 'sharedKey',
+        authType: env('STORAGE_AUTH_TYPE', 'msi'),
         defaultPath: 'uploads',
       },
     },
