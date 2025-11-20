77864de6ae6468ae34fc16f05a91eefd549cc431
Shusil KC <shusilkc@appleexpress.com>
2025-11-20 13:03:52 -0500

    remove storage account integration

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,14 +1,7 @@
-module.exports = ({ env }) => ({
-  upload: {
-    config: {
-      sizeLimit: 500 * 1024 * 1024, // 500 MB
-      provider: "strapi-provider-upload-azure-storage",
-      providerOptions: {
-        account: env("STORAGE_ACCOUNT_NAME"),              
-        accountKey: env("STORAGE_ACCOUNT_KEY"), 
-        containerName: env("BLOB_CONTAINER_NAME"),
-        authType: env("STORAGE_AUTH_TYPE", "default"),
+export default {
+    upload: {
+      config: {
+        sizeLimit: 500 * 1024 * 1024, // 500 MB
       },
     },
-  },
-});
+  };
