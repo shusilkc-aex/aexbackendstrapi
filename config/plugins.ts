20cbc9ceb6ed452cee243f47f768ba5bf3a69e22
Shusil KC <shusilkc@appleexpress.com>
2025-11-20 14:25:33 -0500

    add the default upload path

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,12 +1,14 @@
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      sizeLimit: 500 * 1024 * 1024, // 500 MB
-      provider: 'strapi-provider-upload-azure-storage',
+      sizeLimit: env.int('UPLOAD_SIZE_LIMIT', 500 * 1024 * 1024), // 500 MB default
+      provider: env('UPLOAD_PROVIDER', 'strapi-provider-upload-azure-storage'),
       providerOptions: {
         account: env('STORAGE_ACCOUNT_NAME'),              
         accountKey: env('STORAGE_ACCOUNT_KEY'), 
         containerName: env('BLOB_CONTAINER_NAME'),
+        authType: env('STORAGE_AUTH_TYPE', 'msi'),
+        defaultPath: 'uploads',
       },
     },
   },
