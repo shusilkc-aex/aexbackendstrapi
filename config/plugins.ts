cbf02c6fa8a1c9d4336ebbc32098673fb2d18ffb
Shusil KC <shusilkc@appleexpress.com>
2025-11-20 12:32:18 -0500

    rename the env variables

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,10 +1,11 @@
 module.exports = ({ env }) => ({
   upload: {
     config: {
+      sizeLimit: 500 * 1024 * 1024, // 500 MB
       provider: "strapi-provider-upload-azure-storage",
       providerOptions: {
-        account: env("BLOB_ACCOUNT_NAME"),              
-        accountKey: env("BLOB_ACCOUNT_KEY"), 
+        account: env("STORAGE_ACCOUNT_NAME"),              
+        accountKey: env("STORAGE_ACCOUNT_KEY"), 
         containerName: env("BLOB_CONTAINER_NAME")
       },
     },
