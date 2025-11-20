d4170c5a796656259eacaf02d206b4da08c80d3f
Shusil KC <shusilkc@appleexpress.com>
2025-11-20 13:27:53 -0500

    add storage integration again

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,7 +1,14 @@
-export default {
-    upload: {
-      config: {
-        sizeLimit: 500 * 1024 * 1024, // 500 MB
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      sizeLimit: 500 * 1024 * 1024, // 500 MB
+      provider: 'strapi-provider-upload-azure-storage',
+      providerOptions: {
+        account: env('STORAGE_ACCOUNT_NAME'),              
+        accountKey: env('STORAGE_ACCOUNT_KEY'), 
+        containerName: env('BLOB_CONTAINER_NAME'),
+        authType: env('STORAGE_AUTH_TYPE'),
       },
     },
-  };
+  },
+});
