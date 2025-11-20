cf0b7cb98c7c78fce82c14fbc58ef0502e1bb43e
Shusil KC <shusilkc@appleexpress.com>
2025-11-20 09:58:36 -0500

    configure the azure storage

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,7 +1,13 @@
-export default {
-    upload: {
-      config: {
-        sizeLimit: 500 * 1024 * 1024, // 500 MB
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'strapi-provider-upload-azure-storage',
+      providerOptions: {
+        account: env('AZURE_STORAGE_ACCOUNT'),
+        accountKey: env('AZURE_STORAGE_ACCOUNT_KEY'),
+        container: env('AZURE_STORAGE_CONTAINER'),
+        endpoint: env('AZURE_STORAGE_ENDPOINT'),
       },
     },
-  };
+  },
+});
