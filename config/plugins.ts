31642b47b235a51b52ae0c69f7e857e533278bde
Shusil KC <shusilkc@appleexpress.com>
2025-11-20 14:34:31 -0500

    hardcode sizelimit and provider

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      sizeLimit: env.int('UPLOAD_SIZE_LIMIT', 500 * 1024 * 1024), // 500 MB default
-      provider: env('UPLOAD_PROVIDER', 'strapi-provider-upload-azure-storage'),
+      sizeLimit: 524288000, // 500 MB
+      provider: 'strapi-provider-upload-azure-storage',
       providerOptions: {
         account: env('STORAGE_ACCOUNT_NAME'),              
         accountKey: env('STORAGE_ACCOUNT_KEY'), 
